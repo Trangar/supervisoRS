062e9af94bed8ac7ba02eca5a440c016765d34c0
Victor Koenders <victor.koenders@qrtech.se>
2024-10-17 13:45:38 +0200

    Added an auto detect for factorio paths

--- src/factorio/mod.rs
+++ src/factorio/mod.rs
@@ -1,1 +1,52 @@
+use std::path::PathBuf;
+
+use platform_dirs::AppDirs;
+
 pub mod export;
+
+pub fn find_factorio_install_dir() -> Option<PathBuf> {
+    // Based on https://wiki.factorio.com/Application_directory#Application_directory
+    for dir in [
+        "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Factorio\\bin\\x64",
+        "C:\\Program Files\\Factorio\\bin\\x64",
+    ] {
+        if PathBuf::from(dir).join("Factorio.exe").exists() {
+            return Some(PathBuf::from(dir));
+        }
+    }
+
+    for dir in [
+        // macos
+        "~/Library/Application Support/Steam/steamapps/common/Factorio.app/Contents",
+        "/Applications/Factorio.app/Contents",
+        // linux
+        "~/.steam/steam/steamapps/common/Factorio/bin/x64",
+        "~/.factorio/bin/x64",
+    ] {
+        let dir = shellexpand::tilde(dir).to_string();
+        if PathBuf::from(&dir).join("Factorio").exists() {
+            return Some(PathBuf::from(dir));
+        }
+    }
+
+    None
+}
+
+pub fn find_factorio_config_dir() -> Option<PathBuf> {
+    // Based on https://wiki.factorio.com/Application_directory#User_data_directory
+    let user_dirs = AppDirs::new(Some("Factorio"), false).unwrap();
+    let config_dir = user_dirs.config_dir;
+
+    if config_dir.join("saves").exists() {
+        return Some(config_dir);
+    }
+
+    for dir in ["~/Library/Application Support/factorio", "~/.factorio"] {
+        let dir = shellexpand::tilde(dir).to_string();
+        if PathBuf::from(&dir).join("saves").exists() {
+            return Some(PathBuf::from(dir));
+        }
+    }
+
+    None
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
-use std::path::Path;
-
 mod factorio;
 
 fn main() {
-    let _ = std::fs::remove_dir_all("preset/k2se");
-    std::fs::create_dir_all("preset/k2se").unwrap();
+    let Some(install_dir) = factorio::find_factorio_install_dir() else {
+        println!("Factorio install dir not found");
+        println!("Please update the code to include the correct path on your machine");
+        return;
+    };
 
-    factorio::export::export(factorio::export::ExportArgs {
-        mod_directory: Path::new("C:/Users/VictorKoenders/AppData/Roaming/Factorio"),
-        factorio_dir: Path::new(
-            "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Factorio\\bin\\x64\\",
-        ),
-        output_dir: std::env::current_dir()
-            .unwrap()
-            .join("preset")
-            .join("k2se")
-            .as_path(),
-    });
+    let Some(config_dir) = factorio::find_factorio_config_dir() else {
+        println!("Factorio config dir not found");
+        println!("Please update the code to include the correct path on your machine");
+        return;
+    };
+
+    println!("Factorio install dir: {:?}", install_dir);
+    println!("Factorio config dir: {:?}", config_dir);
+
+    if dialoguer::Confirm::new()
+        .with_prompt("Do you want to export data?")
+        .default(false)
+        .interact()
+        .unwrap()
+    {
+        let _ = std::fs::remove_dir_all("preset/k2se");
+        std::fs::create_dir_all("preset/k2se").unwrap();
+        factorio::export::export(factorio::export::ExportArgs {
+            mod_directory: &config_dir,
+            factorio_dir: &install_dir,
+            output_dir: std::env::current_dir()
+                .unwrap()
+                .join("preset")
+                .join("k2se")
+                .as_path(),
+        });
+    }
 }

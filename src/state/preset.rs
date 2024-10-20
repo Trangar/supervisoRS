2728088083d34dc58246f6818751cde879698e84
Victor Koenders <git@trang.ar>
2024-10-20 10:30:47 +0200

    Validate that a u64 hasher does not cause collisions on k2se

--- src/state/preset.rs
+++ src/state/preset.rs
@@ -5,6 +5,7 @@ use noisy_float::types::R32;
 use rustc_hash::FxHashMap;
 use std::path::PathBuf;
 
+#[derive(Debug)]
 pub struct Preset {
     pub name: String,
     pub recipes: FxHashMap<RecipeId, Recipe>,
@@ -31,29 +32,126 @@ impl Preset {
         println!("  Found {} items", deserialized.item.len());
         println!("  Found {} fluids", deserialized.fluid.len());
 
-        todo!();
+        let mut preset = Preset {
+            name: name.to_string(),
+            recipes: FxHashMap::default(),
+            buildings: FxHashMap::default(),
+            items: FxHashMap::default(),
+            fluids: FxHashMap::default(),
+        };
+
+        for (recipe_name, recipe) in deserialized.recipe {
+            let id = RecipeId::hashed(recipe_name);
+
+            if preset.recipes.contains_key(&id) {
+                panic!(
+                    "Duplicate recipe id: {:?}, {} - {}",
+                    id, preset.recipes[&id].name, recipe_name
+                );
+            }
+            preset.recipes.insert(
+                id,
+                Recipe {
+                    id,
+                    name: recipe_name.to_string(),
+                    ..Default::default()
+                },
+            );
+        }
+
+        for (item_name, item) in deserialized.item {
+            let id = ItemId::hashed(item_name);
+
+            if preset.items.contains_key(&id) {
+                panic!(
+                    "Duplicate item id: {:?}, {} - {}",
+                    id, preset.items[&id].name, item_name
+                );
+            }
+            preset.items.insert(
+                id,
+                Item {
+                    id,
+                    name: item_name.to_string(),
+                    ..Default::default()
+                },
+            );
+        }
+
+        for (fluid_name, fluid) in deserialized.fluid {
+            let id = FluidId::hashed(fluid_name);
+
+            if preset.fluids.contains_key(&id) {
+                panic!(
+                    "Duplicate fluid id: {:?}, {} - {}",
+                    id, preset.fluids[&id].name, fluid_name
+                );
+            }
+            preset.fluids.insert(
+                id,
+                Fluid {
+                    id,
+                    name: fluid_name.to_string(),
+                },
+            );
+        }
+
+        dbg!(preset)
     }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct RecipeId(pub usize);
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
+pub struct RecipeId(pub u64);
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct BuildingId(pub usize);
+impl RecipeId {
+    pub fn hashed(name: &str) -> Self {
+        use std::hash::{Hash, Hasher};
+        let mut hasher = std::hash::DefaultHasher::default();
+        name.hash(&mut hasher);
+        RecipeId(hasher.finish())
+    }
+}
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct ItemId(pub usize);
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
+pub struct BuildingId(pub u64);
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct FluidId(pub usize);
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
+pub struct ItemId(pub u64);
 
-#[derive(Clone, Debug)]
+impl ItemId {
+    pub fn hashed(name: &str) -> Self {
+        use std::hash::{Hash, Hasher};
+        let mut hasher = std::hash::DefaultHasher::default();
+        name.hash(&mut hasher);
+        ItemId(hasher.finish())
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
+pub struct FluidId(pub u64);
+
+impl FluidId {
+    pub fn hashed(name: &str) -> Self {
+        use std::hash::{Hash, Hasher};
+        let mut hasher = std::hash::DefaultHasher::default();
+        name.hash(&mut hasher);
+        FluidId(hasher.finish())
+    }
+}
+
+#[derive(Clone, Debug, Default)]
 pub struct Recipe {
     pub id: RecipeId,
+    pub name: String,
     pub allowed_buildings: Vec<BuildingId>,
     pub inputs: Vec<InOutput>,
     pub outputs: Vec<InOutput>,
-    pub icon: PathBuf,
+}
+
+impl Recipe {
+    pub fn icon(&self, preset_name: &str) -> PathBuf {
+        PathBuf::from(format!("preset/{}/recipe/{}.png", preset_name, self.name))
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -66,8 +164,13 @@ pub struct InOutput {
 pub struct Building {
     pub id: BuildingId,
     pub name: String,
-    pub icon: PathBuf,
     pub speed: R32,
+}
+
+impl Building {
+    pub fn icon(&self, preset_name: &str) -> PathBuf {
+        PathBuf::from(format!("preset/{}/entity/{}.png", preset_name, self.name))
+    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -76,16 +179,26 @@ pub enum ItemOrFluidId {
     Fluid(FluidId),
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct Item {
     pub id: ItemId,
     pub name: String,
-    pub icon: PathBuf,
 }
 
-#[derive(Clone, Debug)]
+impl Item {
+    pub fn icon(&self, preset_name: &str) -> PathBuf {
+        PathBuf::from(format!("preset/{}/item/{}.png", preset_name, self.name))
+    }
+}
+
+#[derive(Clone, Debug, Default)]
 pub struct Fluid {
     pub id: FluidId,
     pub name: String,
-    pub icon: PathBuf,
+}
+
+impl Fluid {
+    pub fn icon(&self, preset_name: &str) -> PathBuf {
+        PathBuf::from(format!("preset/{}/fluid/{}.png", preset_name, self.name))
+    }
 }
